e6556bab4a4c2b70236b25188719038779839b1f
garylee1 <gary@siftscience.com>
2017-05-22 19:07:35 -0700

    (For Janice) Creative use of espresso (#42)
    * Creative use of espresso
    
    * More reliable tests

--- hello-sift/src/androidTest/java/AppendIntegrationTest.java
+++ hello-sift/src/androidTest/java/AppendIntegrationTest.java
@@ -1,10 +1,20 @@
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import com.sift.api.representations.MobileEventJson;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import siftscience.android.Queue;
+import siftscience.android.Sift;
 import siftscience.android.hellosift.HelloSiftTest;
 import siftscience.android.hellosift.R;
 
@@ -28,14 +38,54 @@ public class AppendIntegrationTest {
     @Test
     public void appendDeviceTest() {
         onView(withId(R.id.collect)).perform(click());
-        onView(withId(R.id.deviceDebug)).perform(click());
-        onView(withId(R.id.deviceDebug)).check(matches(withText("empty")));
+        Queue queue = Sift.get().getQueue(Sift.DEVICE_PROPERTIES_QUEUE_IDENTIFIER);
+
+        try {
+            Method m = queue.getClass().getDeclaredMethod("transfer");
+            m.setAccessible(true);
+            List<MobileEventJson> backingQueue = (List<MobileEventJson>) m.invoke(queue);
+
+            if (backingQueue.size() != 0) {
+                onView(withId(R.id.collect))
+                        .check(matches(withText("device properties queue should be flushed but has size " +
+                                backingQueue.size())));
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void appendAppTest() {
         onView(withId(R.id.collect)).perform(click());
-        onView(withId(R.id.appDebug)).perform(click());
-        onView(withId(R.id.appDebug)).check(matches(withText("2 HelloSiftTest foo bar baz https://api3.siftscience.com/v3/accounts/%s/mobile_events")));
+        Queue queue = Sift.get().getQueue(Sift.APP_STATE_QUEUE_IDENTIFIER);
+
+        try {
+            Method m = queue.getClass().getDeclaredMethod("transfer");
+            m.setAccessible(true);
+            List<MobileEventJson> backingQueue = (List<MobileEventJson>) m.invoke(queue);
+
+            if (backingQueue.size() != 2) {
+                onView(withId(R.id.collect))
+                        .check(matches(withText("app state queue should contain 2 items but has size " +
+                                backingQueue.size())));
+            }
+
+            if (!backingQueue.get(0).androidAppState.activityClassName.equals("HelloSiftTest")) {
+                onView(withId(R.id.collect))
+                        .check(matches(withText("activityClassName should be \"HelloSiftTest\" but was " +
+                                backingQueue.get(0).androidAppState.activityClassName)));
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
--- hello-sift/src/main/java/siftscience/android/hellosift/HelloSiftTest.java
+++ hello-sift/src/main/java/siftscience/android/hellosift/HelloSiftTest.java
@@ -6,19 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.common.base.Joiner;
-import com.google.common.collect.Lists;
-import com.sift.api.representations.MobileEventJson;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.List;
 
 import siftscience.android.AppStateCollector;
-import siftscience.android.Queue;
 import siftscience.android.Sift;
 
 public class HelloSiftTest extends AppCompatActivity {
@@ -37,6 +28,7 @@ public class HelloSiftTest extends AppCompatActivity {
         Sift.open(this, new Sift.Config.Builder()
                 .withAccountId("bar")
                 .withBeaconKey("baz")
+                .withDisallowLocationCollection(true)
                 .build());
 
         Sift.get().setUserId("foo");
@@ -60,76 +52,6 @@ public class HelloSiftTest extends AppCompatActivity {
                 }
 
                 Sift.collect();
-            }
-        });
-
-        final TextView deviceDebug = (TextView) findViewById(R.id.deviceDebug);
-
-        deviceDebug.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Queue queue = Sift.get().getQueue(Sift.DEVICE_PROPERTIES_QUEUE_IDENTIFIER);
-
-                try {
-                    Method m = queue.getClass().getDeclaredMethod("transfer");
-                    m.setAccessible(true);
-                    List<MobileEventJson> backingQueue = (List<MobileEventJson>) m.invoke(queue);
-
-                    if (!backingQueue.isEmpty()) {
-                        String result = Joiner.on(" ").join(Lists.newArrayList(Integer.toString(backingQueue.size()),
-                                backingQueue.get(0).androidDeviceProperties.appName,
-                                backingQueue.get(0).userId,
-                                Sift.get().getConfig().accountId,
-                                Sift.get().getConfig().beaconKey,
-                                Sift.get().getConfig().serverUrlFormat
-                        ));
-
-                        deviceDebug.setText(result);
-                    } else {
-                        deviceDebug.setText("empty");
-                    }
-                } catch (NoSuchMethodException e) {
-                    e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        final TextView appDebug = (TextView) findViewById(R.id.appDebug);
-
-        appDebug.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Queue queue = Sift.get().getQueue(Sift.APP_STATE_QUEUE_IDENTIFIER);
-
-                try {
-                    Method m = queue.getClass().getDeclaredMethod("transfer");
-                    m.setAccessible(true);
-                    List<MobileEventJson> backingQueue = (List<MobileEventJson>) m.invoke(queue);
-
-                    if (!backingQueue.isEmpty()) {
-                        String result = Joiner.on(" ").join(Lists.newArrayList(Integer.toString(backingQueue.size()),
-                                backingQueue.get(0).androidAppState.activityClassName,
-                                backingQueue.get(0).userId,
-                                Sift.get().getConfig().accountId,
-                                Sift.get().getConfig().beaconKey,
-                                Sift.get().getConfig().serverUrlFormat
-                        ));
-
-                        appDebug.setText(result);
-                    } else {
-                        appDebug.setText("empty");
-                    }
-                } catch (NoSuchMethodException e) {
-                    e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
-                }
             }
         });
     }
