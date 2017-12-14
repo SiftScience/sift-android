bddb44b950e5808e7eef89335406611879f7370d
garylee1 <gary@siftscience.com>
2017-12-14 14:33:46 -0800

    Remove unused import (#85)

--- hello-sift/src/main/java/siftscience/android/hellosift/HelloSift.java
+++ hello-sift/src/main/java/siftscience/android/hellosift/HelloSift.java
@@ -10,8 +10,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.Collections;
-
 import siftscience.android.Sift;
 
 public class HelloSift extends AppCompatActivity {
